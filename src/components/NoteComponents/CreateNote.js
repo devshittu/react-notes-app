4ff34da5c63bed1684fdde2a400e7da4cd77fcf6
Muhammed Shittu <devshittu@gmail.com>
2023-01-21 13:54:31 +0000

    Added a character counter.

--- src/components/NoteComponents/CreateNote.js
+++ src/components/NoteComponents/CreateNote.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
+const CHAR_LIMIT = 100;
 
 function CreateNote({ textHandler, saveHandler, inputText }) {
+    const charLeft = CHAR_LIMIT - inputText.length;
     return (
         <div className="note" style={{ background: "rgba(255, 255, 255, 0)" }}>
             <textarea
                 cols="10"
                 rows="5"
                 placeholder="Type...."
-                maxLength="100"
+                maxLength="{{ CHAR_LIMIT }}"
                 value={inputText}
                 onChange={textHandler}
             ></textarea>
             <div className="note__footer">
-                <span className="label"> left</span>
+                <span className="label">{charLeft} characters left</span>
                 <button className="note__save" onClick={saveHandler}>Save</button>
             </div>
         </div>
